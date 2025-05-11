7d6483e16581336d437fe6d401df0c29c2700f49
firma <119467443+firma2021@users.noreply.github.com>
2025-05-11 17:03:04 +0800

    Create crtp.cpp

---
+++ crtp.cpp
@@ -0,0 +1,60 @@
+#include <print>
+using namespace std;
+
+template <typename T>
+class UI
+{
+	T* derived()
+	{
+		return static_cast<T*>(this);
+	}
+public:
+	void draw()
+	{
+		derived()->erase_background();
+		derived()->paint();
+	}
+};
+
+class Button : public UI<Button>
+{
+	friend class UI<Button>;
+
+	void erase_background()
+	{
+	    println("erasing button background...");
+	}
+	void paint()
+	{
+	    println("painting button...");
+	}
+};
+
+class CheckBox : public UI<CheckBox>
+{
+    friend class UI<CheckBox>;
+    
+	void erase_background()
+	{
+	    println("erasing checkbox background...");
+	}
+	void paint()
+	{
+	    println("painting checkbox...");
+	}
+};
+
+template <typename T>
+void draw_ui(UI<T>& u)
+{
+	u.draw();
+}
+
+int main()
+{
+	Button b;
+	CheckBox c;
+	
+	draw_ui(b);
+	draw_ui(c);
+}
